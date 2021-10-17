aa2edaf63692d4087c248e3667c325c118157e05
silviu03 <83119817+silviu03@users.noreply.github.com>
2021-10-17 15:20:59 +0300

    update

--- e-commerce.js
+++ e-commerce.js
@@ -1,9 +1,15 @@
-$(function () {
-    const subscribeBtn = $('.subscribe');
+$(function() {
+    let subscribeBtn = $('#subscribe-btn');
+    let closeOverlay = $('.close-subscribe-overlay');
+    let subscribeOverlay = $('.subscribe-overlay');
 
-    subscribeBtn.click(function ( ) {
-        
-    })
-
-
-})
+    subscribeBtn.click(function () {
+        console.log("sdfs")
+        subscribeOverlay.removeClass('hidden');
+    });
+    closeOverlay.click(function () {
+        console.log("dfbdfb")
+        subscribeOverlay.addClass('hidden');
+    });
+    
+});
